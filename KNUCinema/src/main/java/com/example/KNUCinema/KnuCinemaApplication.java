ef2e86d1eb328235ecf56650d7fa6ccd7997976b
sulbos-GP <gs2576@naver.com>
2024-05-26 20:55:27 +0900

    spring Add

---
+++ KNUCinema/src/main/java/com/example/KNUCinema/KnuCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.KNUCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnuCinemaApplication.class, args);
+	}
+
+}
---
+++ KNUCinema/src/test/java/com/example/KNUCinema/KnuCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KNUCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnuCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
