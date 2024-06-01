bddf861b4c29094af63ed93bbdd044dedfee721c
Lacheln1 <psionict@naver.com>
2024-06-01 20:03:59 +0900

    Update KnuCinemaController.java

--- KNUCinema/src/main/java/com/example/KNUCinema/KnuCinemaController.java
+++ KNUCinema/src/main/java/com/example/KNUCinema/KnuCinemaController.java
@@ -29,7 +29,10 @@ public class KnuCinemaController {
 
 
     @RequestMapping("/reserve")
-    public String reservePage(){return "reserve"; }
+    public String reservePage(Model model){
+        model.addAttribute("theaterid", movieReservation.selectTheater();)
+
+        return "reserve"; }
 
 
     @RequestMapping("/Seat/{id}")
