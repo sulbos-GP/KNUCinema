2c938670f165aca558d0456cc7bb66485b5fbac9
Lacheln1 <psionict@naver.com>
2024-06-10 16:10:00 +0900

    404,500 예외 처리 및 버튼 추가

--- KNUCinema/src/main/java/com/example/KNUCinema/KnuCinemaController.java
+++ KNUCinema/src/main/java/com/example/KNUCinema/KnuCinemaController.java
@@ -13,12 +13,17 @@ import javax.xml.crypto.Data;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.RequestDispatcher;
 
 @Controller
 public class KnuCinemaController {
@@ -97,11 +102,16 @@ public class KnuCinemaController {
     public String checkBook(@RequestParam("phoneNumber") String number, Model model){
 
         UserDTO user = movieService.findUser(number);
-        model.addAttribute("UserDatas",user);
-
-        ArrayList<ReservationDTO> reservations = movieReservation.getReservationById(user.getId());
-        model.addAttribute("ReservationDatas", reservations);
+        if (user == null) {
+            model.addAttribute("userNotFound", "true");
+        } else {
+            model.addAttribute("UserDatas", user);
+            ArrayList<ReservationDTO> reservations = movieReservation.getReservationById(user.getId());
+            model.addAttribute("ReservationDatas", reservations);
+        }
+    
         return "checkBook";
+     
     }
 
     @RequestMapping("/count")
@@ -317,6 +327,8 @@ public class KnuCinemaController {
     public String reservation(){
         return "reservation";
     }
+
+
 
 
 
